aae9807e40863491dbd78c92b1d47d7ddfbdb863
Thangbodoiqua <dluy2406@gmail.com>
2025-01-14 21:33:39 +0700

    Add some properties and delete actuator security endpoint

--- src/main/java/com/example/SBUdemy/Contrller/FunRestController.java
+++ src/main/java/com/example/SBUdemy/Contrller/FunRestController.java
@@ -1,10 +1,21 @@
 package com.example.SBUdemy.Contrller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return coachName + " " + teamName;
+    }
     @GetMapping("/")
     public String sayHello(){
         return "Hello";
--- src/main/java/com/example/SBUdemy/SbUdemyApplication.java
+++ src/main/java/com/example/SBUdemy/SbUdemyApplication.java
@@ -2,7 +2,6 @@ package com.example.SBUdemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SbUdemyApplication {
